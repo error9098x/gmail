1691a4f62ca1d100f8ef83ffd7eecb73ab06b8fb
ProCodec <43810146+error9098x@users.noreply.github.com>
2024-03-02 08:58:14 +0000

    constatnt fix

--- utils/constant.py
+++ utils/constant.py
@@ -1,16 +1,23 @@
 import os
-from dotenv import load_dotenv
-from os.path import join, dirname, abspath
+# from dotenv import load_dotenv
+# from os.path import join, dirname, abspath
 
-# Construct the path to the .env file
-dotenv_path = join(dirname(dirname(abspath(__file__))), '.env')
+# # Construct the path to the .env file
+# dotenv_path = join(dirname(dirname(abspath(__file__))), '.env')
 
-# Load the .env file
-load_dotenv(dotenv_path)
+# # Load the .env file
+# load_dotenv(dotenv_path)
 
-# Retrieve environment variables
-SMTP_SERVER_ADDRESS = os.getenv('SMTP_SERVER_ADDRESS')
-PORT = os.getenv('PORT')
-SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
-SENDER_ADDRESS = os.getenv('SENDER_ADDRESS')
+# # Retrieve environment variables
+# SMTP_SERVER_ADDRESS = os.getenv('SMTP_SERVER_ADDRESS')
+# PORT = os.getenv('PORT')
+# SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
+# SENDER_ADDRESS = os.getenv('SENDER_ADDRESS')
 
+import streamlit as st
+
+SMTP_SERVER_ADDRESS = st.secrets['SMTP_SERVER_ADDRESS']
+PORT = st.secrets['PORT']
+SENDER_PASSWORD = st.secrets['SENDER_PASSWORD']
+SENDER_ADDRESS = st.secrets['SENDER_ADDRESS']
+st.write(SMTP_SERVER_ADDRESS)
